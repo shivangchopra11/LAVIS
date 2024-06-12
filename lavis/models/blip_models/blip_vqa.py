017cd061f0c0cd79b27037c0da472c5f1f62e3ea
Shivang Chopra <shivangchopra11@gmail.com>
2024-06-11 21:13:02 -0400

    BLIP VQA GradCam support added

--- lavis/models/blip_models/blip_vqa.py
+++ lavis/models/blip_models/blip_vqa.py
@@ -373,3 +373,54 @@ class BlipVQA(BlipBase):
         model.load_checkpoint_from_config(cfg)
 
         return model
+
+def compute_gradcam(model, sample, tokenized_text, block_num=6):
+    model.text_encoder.base_model.base_model.encoder.layer[
+        block_num
+    ].crossattention.self.save_attention = True
+
+    output = model(sample)
+    print(output)
+    loss = output['loss'].sum()
+
+    model.zero_grad()
+    loss.backward()
+    with torch.no_grad():
+        mask = tokenized_text.attention_mask.view(
+            tokenized_text.attention_mask.size(0), 1, -1, 1, 1
+        )  # (bsz,1,token_len, 1,1)
+        token_length = tokenized_text.attention_mask.sum(dim=-1) - 2
+        token_length = token_length.cpu()
+        # grads and cams [bsz, num_head, seq_len, image_patch]
+        grads = model.text_encoder.base_model.base_model.encoder.layer[
+            block_num
+        ].crossattention.self.get_attn_gradients()
+        cams = model.text_encoder.base_model.base_model.encoder.layer[
+            block_num
+        ].crossattention.self.get_attention_map()
+        # print(cams.shape)
+        # assume using vit with 576 num image patch
+        cams = cams[:, :, :, 1:].reshape(sample['image'].size(0), 12, -1, 30, 30) * mask
+        grads = (
+            grads[:, :, :, 1:].clamp(0).reshape(sample['image'].size(0), 12, -1, 30, 30)
+            * mask
+        )
+
+        gradcams = cams * grads
+        gradcam_list = []
+
+        for ind in range(sample['image'].size(0)):
+            token_length_ = token_length[ind]
+            gradcam = gradcams[ind].mean(0).cpu().detach()
+            # [enc token gradcam, average gradcam across token, gradcam for individual token]
+            gradcam = torch.cat(
+                (
+                    gradcam[0:1, :],
+                    gradcam[1 : token_length_ + 1, :].sum(dim=0, keepdim=True)
+                    / token_length_,
+                    gradcam[1:, :],
+                )
+            )
+            gradcam_list.append(gradcam)
+            
+    return gradcam_list, output
